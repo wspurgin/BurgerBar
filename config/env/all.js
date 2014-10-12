cd615acc97e595d316fc612b8d9f62c67b53b285
Will Spurgin <wspurgin@smu.edu>
2014-10-11 23:20:54 -0500

    Initializing BurgerBar Mean stack

--- config/env/all.js
+++ config/env/all.js
@@ -23,7 +23,7 @@ module.exports = {
 
   // The secret should be set to a non-guessable string that
   // is used to compute a session hash
-  sessionSecret: 'MEAN',
+  sessionSecret: '5974c79c3855047110bc24d83e08c789e317a9c5',
 
   // The name of the MongoDB collection to store sessions in
   sessionCollection: 'sessions',
