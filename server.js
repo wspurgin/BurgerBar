5650beb6bc9aec90afda7d605191fc1fd7f2de39
tbgeorge <tyler.george@live.com>
2014-10-15 13:04:01 -0500

    Started adding routes to server to get the hang of things

--- server.js
+++ server.js
@@ -4,7 +4,19 @@
 var mean = require('meanio');
 
 // Creates and serves mean application
-mean.serve({ /*options placeholder*/ }, function(app, config) {
-	var port = config.https && config.https.port ? config.https.port : config.http.port;
-	console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ')');
+mean.serve({ /*options placeholder*/}, function (app, config) {
+    var port = config.https && config.https.port ? config.https.port : config.http.port;
+    console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ')');
+
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/GUI/index.html');
+    });
+    app.get('/currentOrder', function (req, res) {
+        res.sendFile(__dirname + '/GUI/currentOrder.html');
+    });
+    
+    app.get('/burger2pic', function (req, res) {
+        res.sendFile(__dirname + '/GUI/images/burger2.png');
+    });
 });
+
