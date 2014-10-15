c978f9048cdb7a310fab5a3a05f7ab012decb488
tbgeorge <tyler.george@live.com>
2014-10-15 15:44:38 -0500

    Changed server.js back to original version

--- server.js
+++ server.js
@@ -7,16 +7,5 @@ var mean = require('meanio');
 mean.serve({ /*options placeholder*/}, function (app, config) {
     var port = config.https && config.https.port ? config.https.port : config.http.port;
     console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ')');
-
-    app.get('/', function (req, res) {
-        res.sendFile(__dirname + '/GUI/index.html');
-    });
-    app.get('/currentOrder', function (req, res) {
-        res.sendFile(__dirname + '/GUI/currentOrder.html');
-    });
-    
-    app.get('/burger2pic', function (req, res) {
-        res.sendFile(__dirname + '/GUI/images/burger2.png');
-    });
 });
 
