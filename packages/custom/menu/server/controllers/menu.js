2bf07a2692e7e683382855860b38624834c6318a
tbgeorge <tyler.george@live.com>
2014-10-16 18:04:55 -0500

    fixed menu controller

--- packages/custom/menu/server/controllers/menu.js
+++ packages/custom/menu/server/controllers/menu.js
@@ -9,52 +9,53 @@ var mongoose = require('mongoose'),
   Sauce = mongoose.model('Sauce'),
   Topping = mongoose.model('Topping'),
   Cheese = mongoose.model('Cheese'),
-  Bun = mongoose.model('Bun');
-  // async = require('async'),
-  // config = require('meanio').loadConfig();
+  Bun = mongoose.model('Bun'),
+  async = require('async'),
+  config = require('meanio').loadConfig();
   
 /**
  * Get Full menu
  */
 exports.fullMenu = function(req, res) {
   var menu = {};
+  function callback (itemName, item) {
+    menu[itemName] = item;
+    if (Object.keys(menu).length === 6) {
+      return res.json(menu);
+    }
+  }
 
-  Meat.find()
-  .exec(function(err, meats){
+  Meat.find({},function(err, meats){
     if (err) res.send(err);
-    else menu.meats = meats;
+    else callback('meats', meats);
   });
 
   Side.find()
-  .exec(function(err, side){
+  .exec(function(err, sides){
     if (err) res.send(err);
-    else menu.side = side;
+    else callback('sides', sides);
   });
 
-  Sauce.find()
-  .exec(function(err, sauce){
+  Sauce.find(function(err, sauces){
     if (err) res.send(err);
-    else menu.side = sauce;
+    else callback('sauces', sauces);
   });
 
   Topping.find()
   .exec(function(err, topping){
     if (err) res.send(err);
-    else menu.topping = topping;
+    else callback('topping', topping);
   });
 
   Cheese.find()
-  .exec(function(err, cheese){
+  .exec(function(err, cheeses){
     if (err) res.send(err);
-    else menu.cheese = cheese;
+    else callback('cheeses', cheeses);
   });
 
-  Bun.find()
-  .exec(function(err, bun){
+  Bun.find({}, function(err, buns) {
     if (err) res.send(err);
-    else menu.bun = bun;
+    else callback('buns', buns);
   });
 
-  // Other menu items here
-  res.status(200).json(menu);
 };
