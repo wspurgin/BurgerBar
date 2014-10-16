2e7dd7250c3a0ccd48ef1add57807fa5c48005be
William O'Connor <woconnor@smu.edu>
2014-10-16 10:23:56 -0500

    removed weird route

--- packages/custom/menu/server/routes/menu.js
+++ packages/custom/menu/server/routes/menu.js
@@ -5,8 +5,5 @@ var menu = require('../controllers/menu');
 module.exports = function(Menu, app, auth, database, passport) {
   
   app.get('/menu', menu.fullMenu);
-
-
-  app.get('/', index.render);
     
 };
