802481db990a3b5280dba2774b3385e8eb43bbb9
Will Spurgin <wspurgin@smu.edu>
2014-10-17 22:53:30 -0500

    Order controller

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -4,4 +4,79 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
   function($scope, Global) {
     $scope.global = Global;
   }
+])
+.controller('OrderController', ['$scope', '$rootScope', '$http', '$location', '$q', 'Global',
+  function($scope, $rootScope, $http, $location, $q, Global) {
+    $scope.global = Global;
+    $scope.order = {
+      burgers: [],
+      sides: []
+    };
+
+    function Burger(meat, bun, sauces, toppings, cheeses, price) {
+      this.meat = meat;
+      this.bun = bun;
+      this.sauces = sauces;
+      this.toppings = toppings;
+      this.cheeses = cheeses;
+      this.price = price;
+    }
+
+    function getMenu() {
+      $http.get('/menu')
+        .success(function(response) {
+          $scope.menu = response;
+        })
+        .error(function() {
+          alert('Couldn\'t get menu data. Please check your network connection.');
+        });
+    }
+
+    function getLastOrder() {
+      $http.get('/users/me/last-order')
+        .success(function(response) {
+          $scope.lastOrder = response;
+        })
+        .error(function() {
+          console.log('Could not get Users last order');
+        });
+    }
+
+    getMenu();
+    getLastOrder();
+
+    $scope.placeOrder = function() {
+      console.log('Placeing Order');
+      $http.post('/users/me/new-order', {
+        order: $scope.order
+      })
+      .success(function() {
+        alert('Thank you for your order!');
+        $location.url('/');
+      })
+      .error(function() {
+        alert('Could not place order');
+      });
+    };
+    
+    $scope.addBurgerToOrder = function() {
+      var burger = new Burger(
+          $scope.burger.meat,
+          $scope.burger.bun,
+          $scope.burger.sauces,
+          $scope.burger.toppings,
+          $scope.burger.cheeses,
+          $scope.burger.price
+        );
+      $scope.order.burgers.push(burger);
+    };
+
+    $scope.addSideToOrder = function() {
+      var side = {
+        name: $scope.side.name,
+        price: $scope.side.price
+      };
+      $scope.order.sides.push(side);
+    };
+}
 ]);
--- packages/system/public/controllers/order.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('mean.system').controller('OrderController', ['$scope', 'Global',
-  function($scope, Global) {
-    $scope.global = Global;
-  }
-]);
